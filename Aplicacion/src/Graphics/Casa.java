5e741ea38c3f25784607ed50d97c5b118def656b
marca <marca@DESKTOP-1N0GKU2>
2025-03-06 05:14:41 -0600

    Unidad 2 Actividad 31 --- "Modelado de Casa con Graphics"

--- Aplicacion/src/Graphics/Casa.java
+++ Aplicacion/src/Graphics/Casa.java
@@ -25,13 +25,13 @@ public class Casa extends JFrame {
         super.paint(g);
         Graphics2D g2 = (Graphics2D) g;
        
-        g2.setColor(new Color(255, 217, 236));      
+        g2.setColor(new Color(255, 217, 236)); //Color Fondo (oscuro)      
         g2.fillOval(-400, -400, 2500, 2500);
         
-        g2.setColor(new Color(255, 201, 228));      
+        g2.setColor(new Color(255, 201, 228)); //Color Fondo (Medio)    
         g2.fillOval(-80, -80, 2500, 2500);
         
-        g2.setColor(new Color(255, 163, 209));
+        g2.setColor(new Color(255, 163, 209)); //Color Fondo (claro)  
         g2.fillOval(200, 100, 1700, 1700);
         
         g2.setColor(new Color(171, 145, 84)); // Tronco
@@ -44,7 +44,7 @@ public class Casa extends JFrame {
         g2.fillOval(300, 100, 300, 150);
         g2.fillOval(200, 10, 300, 250);
         
-        g2.setColor(new Color(160, 219, 121)); // Hojas Arbol
+        g2.setColor(new Color(160, 219, 121)); // Hojas Arbol (sombra)
         g2.fillOval(300, 205, 180, 180);
         g2.fillOval(300, 105, 280, 130);
         g2.fillOval(200, 15, 260, 230);
@@ -55,7 +55,7 @@ public class Casa extends JFrame {
         g2.setColor(new Color(157, 229, 120)); // color verde piso
         g2.fillArc(-100, 200, 3000, 2500, 0, 180);        
         
-        g2.setColor(new Color(129, 204, 70));
+        g2.setColor(new Color(129, 204, 70)); // cupula (sombra)
         g2.fillOval(610, 300, 900, 400);
 
         g2.setColor(new Color(237, 228, 150)); // color amarillo
@@ -70,23 +70,24 @@ public class Casa extends JFrame {
         g2.setColor(new Color(161, 74, 8)); //puerta
         g2.fillArc(650, 342, 100, 400, 5, 180);
         
-        g2.setColor(new Color(117, 66, 27));
+        g2.setColor(new Color(117, 66, 27)); // cupula
         g2.fillArc(670, 349, 80, 380, 5, 179);
         
-        int[] xs = {780, 790, 980, 1000};
+        int[] xs = {780, 790, 980, 1000}; // cupula (ventana)
         int[] ys = {430, 360, 300, 350};
         g2.fillPolygon(xs, ys, 4);
         
-        g2.setColor(new Color(71, 35, 8));
+        g2.setColor(new Color(71, 35, 8)); //cupula (ventana interior)
         int[] xs_7 = {853, 843, 980,1000};
         int[] ys_7 = {400, 350, 305, 350};
         g2.fillPolygon(xs_7, ys_7, 4);
         
-        g2.setColor(new Color(245, 237, 182));
+        g2.setColor(new Color(245, 237, 182)); // cupula (ventana marco)
         int[] xs_4 = {780, 800, 996, 1000};
         int[] ys_4 = {430, 400, 340, 350};
         g2.fillPolygon(xs_4, ys_4, 4);
-        int[] xs_5 = {780, 790, 800, 810};
+        
+        int[] xs_5 = {780, 790, 800, 810}; // cupula (ventana profundidad)
         int[] ys_5 = {430, 360, 400, 405};
         g2.fillPolygon(xs_5, ys_5, 4);
       
